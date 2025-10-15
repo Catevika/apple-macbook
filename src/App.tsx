bc861c4d619cfdd70037d003807cea5cfcfd8d90
Catevika <dominique.bello@outlook.fr>
2025-10-15 14:15:19 +0200

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
 	return (
-		<h1 className='text-3xl font-bold underline text-indigo-700'>
-			Hello Macbook Pro!
-		</h1>
+		<>
+			<h1 className='text-3xl font-bold underline text-indigo-700'>
+				Hello Macbook Pro!
+			</h1>
+			<Navbar />
+		</>
 	);
 };
 
